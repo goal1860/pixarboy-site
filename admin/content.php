f7837e5aa70c322ccb06653741a79f9412c6fb93
Liang He <lhe@obsidiansecurity.com>
2025-11-09 09:45:03 -0800

    Fix content page style

--- admin/content.php
+++ admin/content.php
@@ -289,20 +289,20 @@ include __DIR__ . '/../includes/header.php';
                                 <div class="admin-content-actions-container">
                                     <?php if ($c['status'] === 'published'): ?>
                                         <a href="/post/<?php echo htmlspecialchars($c['slug']); ?>" class="btn btn-secondary btn-sm admin-content-action-btn" target="_blank" title="View Post">
-                                            <svg class="admin-content-action-icon" fill="currentColor" viewBox="0 0 20 20">
+                                            <svg class="admin-content-action-icon" width="12" height="12" fill="currentColor" viewBox="0 0 20 20">
                                                 <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"/>
                                                 <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"/>
                                             </svg>
                                         </a>
                                     <?php endif; ?>
                                     <a href="?action=edit&id=<?php echo $c['id']; ?>" class="btn btn-primary btn-sm admin-content-action-btn" title="Edit">
-                                        <svg class="admin-content-action-icon" fill="currentColor" viewBox="0 0 20 20">
+                                        <svg class="admin-content-action-icon" width="12" height="12" fill="currentColor" viewBox="0 0 20 20">
                                             <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/>
                                         </svg>
                                     </a>
                                     <a href="?action=delete&id=<?php echo $c['id']; ?>" class="btn btn-danger btn-sm admin-content-action-btn" 
                                        data-confirm="Are you sure you want to delete '<?php echo htmlspecialchars($c['title']); ?>'?" title="Delete">
-                                        <svg class="admin-content-action-icon" fill="currentColor" viewBox="0 0 20 20">
+                                        <svg class="admin-content-action-icon" width="12" height="12" fill="currentColor" viewBox="0 0 20 20">
                                             <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/>
                                         </svg>
                                     </a>
